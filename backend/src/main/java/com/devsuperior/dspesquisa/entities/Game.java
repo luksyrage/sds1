0c17c5e9d4d28b61e1f867fe1b53bd0be474d885
luksyrage <luksy.rage@outlook.com>
2020-09-15 23:52:24 -0300

    Segundo commit, parte da primeira aula

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/entities/Game.java
@@ -0,0 +1,11 @@
+package com.devsuperior.dspesquisa.entities;
+
+import com.devsuperior.dspesquisa.entities.enums.Platform;
+
+public class Game {
+	private Long id;
+	private String title;
+	private Platform platform;
+	
+	private Genre genre;
+}
